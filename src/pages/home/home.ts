159942f21464d28c51b71039b76630f192b66053
Juan Perez <juanlxboxes@gmail.com>
2019-01-26 17:41:55 -0400

    cambios

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, LoadingController, AlertController } from 'ionic-angular';
 import { RProvider } from '../../providers/r/r';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'page-home',
@@ -8,14 +9,180 @@ import { RProvider } from '../../providers/r/r';
 })
 export class HomePage {
   public segmentB: string ='test1';
+  public submitAttempt1 = false;
+  public submitAttempt2 = false;
+  form1: FormGroup;
+  form2: FormGroup;
+  public res1 = '';
+  public res2 = '';
+  public normal: any;
+  public normaln: any;
+  public case;
   public slides = [{id: "test1"}, {id: "test2"}];
   
   constructor(public navCtrl: NavController,
-    public rProvvider: RProvider) {
+    public rProvvider: RProvider,
+    public formBuilder: FormBuilder,
+    public loadingCtrl : LoadingController,
+    public alertController: AlertController) {
+    this.case = "simpson"
+ 
 
+  this.form1 = formBuilder.group({
+  case: ['simpson'],
+  caset: [1],
+  valora: ['',[ Validators.required]],
+  valorb: ['',[ Validators.required]],
+  valorn: [20,[ Validators.required]],
+  });
+
+  this.form2 = formBuilder.group({
+    case: ['simpson'],
+    caset: [1],
+    valorx: ['',[
+      Validators.required
+  
+    ]],
+    valorn: [20,[ Validators.required]],
+    });
   }
   onSegmentChanged(segmentButton) {
 
 
-}
+  } 
+
+  calcularAB(){
+    let loading = this.loadingCtrl.create({
+      spinner: 'hide',
+      content: 'Cargando..'
+    });
+    this.submitAttempt1 = true;
+    console.log(this.form1.value);
+    if(!this.form1.valid){
+    
+    }else{
+
+      if (this.form1.value['case'] == 'simpson' 
+      && this.form1.value['caset'] == 1
+      && ((this.form1.value['valorn'] %2) !=0)){
+        let alert =  this.alertController.create(
+          {
+          message: 'La n debe ser par.',
+          buttons: ['OK']
+        });
+    
+         alert.present();
+      }else{
+        let caso = this.form1.value['case'];
+        let casot = this.form1.value['caset'];
+        let a = this.form1.value['valora'];
+        let b = this.form1.value['valorb'];
+        let n = this.form1.value['valorn'];
+         if (caso =='simpson'){
+         loading.present();
+         if (casot == "1"){
+          this.rProvvider.getProbSimpson(b,a,n).subscribe((res : any) =>{
+            console.log(res);
+            this.res1 = res.response;
+            loading.dismiss()
+          })
+         }else{
+          this.rProvvider.getProbSimpsonpy(b,a,n).subscribe((res : any) =>{
+            console.log(res);
+            this.res1 = res.response;
+            loading.dismiss()
+          })
+         }
+      
+         }else{
+         loading.present();
+         this.rProvvider.getProbTrapecio(b,a,n).subscribe((res : any) =>{
+         console.log(res);
+         this.res1 = res.response;
+         loading.dismiss()
+         })
+         }
+      }
+   
+    }
+  }
+  calcularAcumulada(){
+    let loading = this.loadingCtrl.create({
+      spinner: 'hide',
+      content: 'Cargando..'
+    });
+    this.submitAttempt2 = true;
+    console.log(this.form2.value);
+    if(!this.form2.valid){
+      //console.log(this.form2.controls.valora.valid)
+    }else{
+      if (this.form2.value['case'] == 'simpson' 
+      && this.form2.value['caset'] == 1
+      && ((this.form2.value['valorn'] %2) !=0)){
+        let alert =  this.alertController.create(
+          {
+          message: 'La n debe ser par.',
+          buttons: ['OK']
+        });
+    
+         alert.present();
+      }else{
+     let caso = this.form2.value['case'];
+     let b = this.form2.value['valorx'];
+     let casot = this.form2.value['caset'];
+     let n = this.form2.value['valorn'];
+
+
+      if (caso =='simpson'){
+        //
+
+        loading.present();
+        if (casot == "1"){
+         this.rProvvider.getProbSimpson(b,-20,n).subscribe((res : any) =>{
+           console.log(res);
+           this.res2 = res.response;
+           loading.dismiss()
+         })
+        }else{
+         this.rProvvider.getProbSimpsonpy(b,-20,n).subscribe((res : any) =>{
+           console.log(res);
+           this.res2 = res.response;
+           loading.dismiss()
+         })
+        }
+      }else{
+        loading.present();
+        this.rProvvider.getProbTrapecio(b,-20,n).subscribe((res : any) =>{
+          console.log(res);
+          this.res2 = res.response;
+          loading.dismiss()
+        })
+      }
+    }
+  }
+  }
+  normalTable(){
+ 
+    let loading = this.loadingCtrl.create({
+      spinner: 'hide',
+      content: 'Cargando..'
+    });
+    loading.present();
+  
+    this.rProvvider.getnormalTable().subscribe((r: any) =>{
+      console.log(r);
+      this.normal = r.response;
+  
+      this.rProvvider.getnormalTablen().subscribe((r: any) =>{
+        console.log(r);
+        this.normaln = r.response;
+
+        loading.dismiss();
+  
+      })
+    })
+
+  }
+
+
 }
--- src/providers/r/r.ts
+++ src/providers/r/r.ts
@@ -1,6 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
+import  'rxjs/add/operator/catch';
+
+import  'rxjs/add/operator/map';
 /*
   Generated class for the RProvider provider.
 
@@ -9,12 +12,25 @@ import { Injectable } from '@angular/core';
 */
 @Injectable()
 export class RProvider {
+  baseUrl:string = "http://148.72.23.0:3000";
 
   constructor(public http: HttpClient) {
     console.log('Hello RProvider Provider');
   }
-  normalTable(){
-    
-  }
 
+  public getProbSimpson(b,a=-20,n=20) {
+   return  this.http.get(this.baseUrl + '/api/normal/simpson?a='+a+'&b='+b+'&n='+n);
+  }
+  public getProbSimpsonpy(b,a=-20,n=20) {
+    return  this.http.get(this.baseUrl + '/api/normal/simpsonpy?a='+a+'&b='+b+'&n='+n);
+   }
+  public  getProbTrapecio(b,a=-20,n=20) {
+    return  this.http.get(this.baseUrl + '/api/normal/trapecio?a='+a+'&b='+b+'&n='+n);
+   }
+   public  getnormalTable() {
+    return  this.http.get(this.baseUrl + '/api/normal/table');
+   }
+   public  getnormalTablen() {
+    return  this.http.get(this.baseUrl + '/api/normal/tablen');
+   }
 }
